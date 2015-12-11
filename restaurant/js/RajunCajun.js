de808a6ec24bfc95c3562d24bbe863ccd4c8cbce
Robert <oconn270@gmail.com>
2015-12-11 09:57:52 -0600

    latest

--- restaurant/js/RajunCajun.js
+++ restaurant/js/RajunCajun.js
@@ -1,10 +1,19 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-
-<script>
-$(document).ready(function(){
-	$(".SubTandoori").hide();
-    $(".MenuTandoori h2").click(function(){
-        $(".SubTandoori").toggle();
-    });
+$('nav a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 500);
+    return false;
 });
-</script>
+
+$(document).ready(function(){
+
+    $(".SubMenu").hide();
+    $(".TopMenu h2").click(function(){
+        $(this).closest(".TopMenu").find(".SubMenu").toggle();
+    });
+    $(".close").click(function(){
+    	$(this).closest("ul").hide();
+    });
+
+});
+
